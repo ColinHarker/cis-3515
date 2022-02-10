bc8d764f549548186a3a86ad13af3214c5ab4afc
Colin Harker <53018170+ColinHarker@users.noreply.github.com>
2022-02-10 14:39:50 -0500

    Added spinner with different colors

---
+++ Lab3/app/src/main/java/edu/temple/coloractivity/ColorAdapter.kt
@@ -0,0 +1,51 @@
+package edu.temple.coloractivity
+
+import android.content.Context
+import android.graphics.Color
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+
+class ColorAdapter(_context: Context, _colors: Array<String>) : BaseAdapter() {
+
+    var selectedItem: Int = -1
+    private val context = _context
+    private val colors = _colors
+
+    override fun getCount(): Int {
+        return colors.size
+    }
+
+    override fun getItem(p0: Int): Any {
+        return colors[p0];
+    }
+
+    override fun getItemId(p0: Int): Long {
+        return p0.toLong()
+    }
+
+    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+        val textView : TextView
+
+        if (p1 == null) {
+            textView = TextView(context)
+            textView.textSize = 24f
+            textView.setPadding(10, 10,0,10)
+        } else
+            textView = p1 as TextView
+
+        textView.text = colors[p0]
+        textView.setBackgroundColor(Color.parseColor("white"))
+        return textView
+    }
+
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view = getView(position, convertView, parent)
+
+        view.setBackgroundColor(Color.parseColor(colors[position]))
+
+        return view
+    }
+
+}
