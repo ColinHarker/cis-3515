57baeb710dfee0d26440554b4422f31f3105a2da
colin <colinharker5@yahoo.com>
2022-02-17 12:19:29 -0500

    Additions

--- 2_15_22/app/src/main/java/edu/temple/myactivityproj/MainActivity2.kt
+++ 2_15_22/app/src/main/java/edu/temple/myactivityproj/MainActivity2.kt
@@ -3,6 +3,7 @@ package edu.temple.myactivityproj
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 
 class MainActivity2 : AppCompatActivity() {
@@ -18,5 +19,34 @@ class MainActivity2 : AppCompatActivity() {
             setResult(RESULT_OK, resultIntent)
             finish()
         }
+
+        findViewById<Button>(R.id.button2).setOnClickListener{
+            startActivity(Intent(this, MainActivity2::class.java))
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("Fired", "ActivityB - onStart()")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("Fired", "ActivityB - onResume()")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("Fired", "ActivityB - onPause()")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("Fired", "ActivityB - onStop()")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Fired", "oActivityB - nDestroy()")
     }
 }
